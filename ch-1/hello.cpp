8dae43045258953ae7ac65d7d3d1354b5ba8190f
leesper <pascal7718@gmail.com>
2022-01-13 22:23:24 +0800

    feat: Add multiple files to test camke

---
+++ ch-1/hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "hello.h"
+using namespace std;
+
+void hello() {
+    cout << "HELLO MULTIPLE FILES" << endl;
+}
+
--- ch-1/hello_world.cpp
+++ ch-1/hello_world.cpp
@@ -3,10 +3,12 @@
 //
 
 #include <iostream>
+#include "hello.h"
 using namespace std;
 
 int main() {
     cout << "Hello World" << endl;
+    hello();
     return 0;
 }
 
