3bc21894fde74681e9d23f0f42555b11a9b42e28
leesper <pascal7718@gmail.com>
2022-07-24 16:28:00 +0800

    binary search version of 55 score

--- final_dsa/range.cpp
+++ final_dsa/range.cpp
@@ -2,9 +2,15 @@
 // Created by 李科君 on 2022/7/23.
 //
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
 int range(int *pInt, int n, int a, int b);
+int comp(const void *a, const void *b);
+
+int *find_first_gte(int *pInt, int l, int r, int t);
+
+int *find_first_lte(int *pInt, int l, int r, int t);
 
 int main() {
     int n, m;
@@ -33,17 +39,52 @@ int main() {
 }
 
 int range(int *pInt, int n, int a, int b) {
-    int *ptr = pInt;
-    while (*ptr < a && ptr < pInt + n) {
-        ptr++;
-    }
+    qsort(pInt, n, sizeof(int), comp);
 
-    int count = 0;
-    for (; ptr < pInt + n; ptr++) {
-        if (a <= *ptr && *ptr <= b) {
-            count++;
+    int *p1 = find_first_gte(pInt, 0, n-1, a);
+    int *p2 = find_first_lte(pInt, 0, n-1, b);
+
+    return (p2 - p1) + 1;
+}
+
+int *find_first_lte(int *pInt, int l, int r, int t) {
+    int *pos = nullptr;
+
+    while (l <= r) {
+        int m = (l + r) / 2;
+        if (t == pInt[m]) {
+            pos = pInt + m;
+            break;
+        } else if (t < pInt[m]) {
+            r = m - 1;
+            pos = pInt + r;
+        } else {
+            l = m + 1;
+            pos = pInt + m;
         }
     }
+    return pos;
+}
 
-    return count;
+int *find_first_gte(int *pInt, int l, int r, int t) {
+    int *pos = nullptr;
+
+    while (l <= r) {
+        int m = (l + r) / 2;
+        if (t == pInt[m]) {
+            pos = pInt + m;
+            break;
+        } else if (t < pInt[m]) {
+            r = m - 1;
+            pos = pInt + m;
+        } else {
+            l = m + 1;
+            pos = pInt + l;
+        }
+    }
+    return pos;
+}
+
+int comp(const void *a, const void *b) {
+    return *(int*)a - *(int*)b;
 }
